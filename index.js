436d3eb279642804c0f1f2b94e7a2b30bb161fc7
Minhazul Ashim <ashim.ashim7470@gmail.com>
2021-10-29 05:05:37 +0600

    first api developed

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ptzya.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+console.log(uri)
 //Middlewares
 
 app.use(cors());
 app.use(express.json());
 
+//Database functionalities
+
+async function run() {
+
+    try {
+
+        await client.connect();
+
+        const database = client.db('travello')
+        const destinationCollection = database.collection('destinations');
+
+        //GET API for retrieving all the destinations
+        app.get('/destinations', async (req, res) => {
+
+            const cursor = destinationCollection.find({});
+            const result = await cursor.toArray();
+
+            res.send(result);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir)
+
 app.get('/', (req, res) => {
 
-    res.send('The server is live and active')
+    res.send('The server is live')
 })
 
 app.listen(port, () => {
